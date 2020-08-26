843ebde73077adb5ca956a40c7dd7fbf99ea1922
marcello <marcello@dcide.com.br>
2020-08-26 08:14:55 -0300

    fixing php

--- home.php
+++ home.php
@@ -1,14 +1,13 @@
-<!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>Flask Tutorial</title>
+    <title>Form</title>
   </head>
   <body>
-    <?php include_once('template.html') ?>
-    <main> 
+  <?php include_once('template.html') ?>
+  <main>
     <h1> My First Try Using Flask </h1>
     <p> Flask is Fun </p>
-    </main>
+  </main>
   </body>
 </html>
+
